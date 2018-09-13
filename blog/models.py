ffd84d1ec4c4a42f7aaab44737aa470593696728
codyByCherry <373466869@qq.com>
2018-09-13 10:43:57 +0800

    生成数据库文件

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 # Create your models here.
 class Category(models.Model):
@@ -22,9 +22,17 @@ class Post(models.Model):
     body = models.TextField()
     excerpt = models.CharField(max_length=255)
 
+    created_time = models.DateTimeField(default=timezone.now)
+    modified_time = models.DateTimeField(auto_now=True)
+
     category= models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.modified_time = timezone.now()
+        super(Post, self).save(force_insert, force_update, using, update_fields)
 
     def __str__(self):
         return self.title
