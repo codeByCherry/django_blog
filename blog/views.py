97dffa7107617aa89ec5309a5d01e255f8c4e84e
codyByCherry <373466869@qq.com>
2018-09-13 11:29:04 +0800

    add static files

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Post
+
 
 # Create your views here.
 def index(request):
+
+    # 最新的 post 在前面显示
+    post_list = Post.objects.all().order_by('-created_time')
+
     context = dict(
-        title='Welcome',
-        content='Hi, here',
+        post_list=post_list,
     )
 
     return render(request, 'blog/index.html', context=context)
