3cc727f72d52348b2fc281a9132907c803622484
codyByCherry <373466869@qq.com>
2018-09-13 10:53:53 +0800

    增加逻辑删除，取消物理删除功能

--- blog/models.py
+++ blog/models.py
@@ -29,10 +29,16 @@ class Post(models.Model):
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    is_delete = models.BooleanField(default=False)
+
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         self.modified_time = timezone.now()
         super(Post, self).save(force_insert, force_update, using, update_fields)
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_delete = True
+        self.save()
 
     def __str__(self):
         return self.title
