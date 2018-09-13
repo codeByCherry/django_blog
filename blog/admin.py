887e61aa012c4f2f4c808107ee50dac9c6e05882
codyByCherry <373466869@qq.com>
2018-09-13 14:05:39 +0800

    设置blog 的管理后台

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
 
