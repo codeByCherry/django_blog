f2f9ba84317044674de61c4a7d43d5b60c186319
codyByCherry <373466869@qq.com>
2018-09-14 10:41:00 +0800

    编写 归档和分类的跳转逻辑

--- blog/urls.py
+++ blog/urls.py
@@ -22,4 +22,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:post_id>/', views.detail, name='detail'),
+    path('archives/<int:year>/<int:month>/', views.archives, name='archives'),
+    path('category/<int:category_id>', views.category, name='category'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
 from .models import Post
+from .models import Category
 import markdown
 
 # Create your views here.
+# 显示所有文章列表
 def index(request):
 
     # 最新的 post 在前面显示
@@ -17,6 +19,29 @@ def index(request):
 
     return render(request, 'blog/index.html', context=context)
 
+
+# 按时间显示文章
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month,
+                                    is_delete = False
+                                    ).order_by('-created_time')
+
+    context = dict(
+        post_list=post_list,
+    )
+
+    return render(request, 'blog/index.html', context=context)
+
+
+def category(request, category_id):
+    cate = get_object_or_404(Category, pk=category_id)
+    post_list = Post.objects.filter(category=cate)
+    context = dict(
+        post_list=post_list,
+    )
+
+    return render(request, 'blog/index.html', context=context)
 
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
